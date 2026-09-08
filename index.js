84fea5aacf17d45216e45f53aa9c94dbfe886def
Rittik <rittikchakraborty24@gmail.com>
2026-09-08 10:43:45 +0530

    Factory

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1, 
-        y: 1
-    },
-    draw: function () {
-        console.log("Draw");
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1, 
+//         y: 1
+//     },
+//     draw: function () {
+//         console.log("Draw");
         
-    }
+//     }
+// }
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log("Draw by Factory");
+        }
+    };
 }
+
+const circle = createCircle(1);
 
 circle.draw();
